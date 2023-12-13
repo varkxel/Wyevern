6515b56d335f6a42525bb1cd83e2344f7c6f03ec
varkxel <89874295+varkxel@users.noreply.github.com>
2023-12-13 02:27:35 +0000

    Store job class in JobHandle, plus Schedule()

--- JobSystem/JobSystem.hpp
+++ JobSystem/JobSystem.hpp
@@ -1,6 +1,8 @@
 #ifndef WYEVERN_JOBSYSTEM_INCLUDED
 #define WYEVERN_JOBSYSTEM_INCLUDED
 
+#include "Job.hpp"
+
 #include <any>
 #include <new>
 #include <functional>
@@ -13,36 +15,55 @@ namespace Wyevern
 	class JobSystem final
 	{
 	public:
-		explicit JobSystem(unsigned threads = 0u);
-
-	private:
 		struct
 			alignas(std::hardware_constructive_interference_size)
-			JobData
+			JobHandle
 		{
-			/// <summary>
-			/// The job function to call.
-			/// </summary>
-			std::function<void()> function;
-
-			/// <summary>
-			/// Job data stored here.
-			/// </summary>
-			std::any data;
-
 			/// <summary>
 			/// The job which is this job's parent.
 			/// </summary>
-			std::optional<std::shared_ptr<JobData>> parent;
-			
+			std::optional<std::shared_ptr<JobHandle>> parent;
+
 			/// <summary>
 			/// The amount of unfinished children this job has.
 			/// </summary>
 			std::atomic_int children;
+
+			/// <summary>
+			/// Job data/function pointer.
+			/// </summary>
+			std::unique_ptr<Job> data;
 		};
+
+		explicit JobSystem(unsigned threads = 0u);
+
+		template<typename JobType>
+		std::shared_ptr<JobHandle> Schedule(const Job& job, std::optional<std::shared_ptr<JobHandle>> parent = std::nullopt)
+		{
+			std::shared_ptr<JobSystem::JobHandle> handle = std::make_shared<JobSystem::JobHandle>();
+			if (parent.has_value())
+			{
+				++(parent.value()->children);
+			}
+			handle->parent = parent;
+			handle->children = 0;
+			handle->data = std::make_unique<JobType>(job);
+			return handle;
+		}
+	private:
+		/// <summary>
+		/// Function that runs in each worker thread.
+		/// </summary>
 		static void WorkerThread();
 
+		/// <summary>
+		/// Total number of threads allocated to the JobSystem.
+		/// </summary>
 		unsigned threadCount;
+
+		/// <summary>
+		/// The thread definitions/handles.
+		/// </summary>
 		std::vector<std::thread> threads;
 	};
 }
