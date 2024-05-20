abe52fbb15c162c02f9d5ee513eb500604278286
varkxel <89874295+varkxel@users.noreply.github.com>
2024-05-20 20:07:38 +0100

    Modules

---
+++ Module.hpp
@@ -0,0 +1,42 @@
+#ifndef WYEVERN_MODULE_INCLUDED
+#define WYEVERN_MODULE_INCLUDED
+
+#include <string>
+#include <dlfcn.h>
+#include <stdexcept>
+#include <iostream>
+#include <memory>
+
+namespace Wyevern {
+	template<typename TModule, const char* entryPoint, const char* exitPoint>
+	class Module {
+	protected:
+		const std::string path;
+		void* handle;
+		
+	public:
+		explicit Module(const std::string& path) : path(path) {
+			handle = dlopen(path.c_str(), RTLD_NOW);
+			if(handle == nullptr) {
+				throw std::runtime_error(dlerror());
+			}
+		}
+		
+		~Module() {
+			int status = dlclose(handle);
+			if(status != 0) {
+				std::cerr << dlerror() << std::endl;
+			}
+		}
+		
+		std::shared_ptr<TModule> Instance() {
+			using EntryFunction = TModule* (*) ();
+			using ExitFunction = void (*) (TModule*);
+			
+			auto entry = reinterpret_cast<EntryFunction>(dlsym(handle, entryPoint));
+			auto exit = reinterpret_cast<ExitFunction>(dlsym(handle, exitPoint));
+		}
+	};
+}
+
+#endif
